d73751cd6330a08cfc3a1c41728449ed0aa753c0
Alex Flórez <alex.josueff@gmail.com>
2018-09-06 09:47:03 -0300

    Interface to set server. Post image

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ import kivy
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
 from kivy.uix.popup import Popup
 from kivy.logger import Logger
+from kivy.properties import StringProperty
 
 from plyer import camera
 
@@ -18,29 +20,41 @@ def name_imgs():
     return "{}_{}".format(prefix, name)
 
 class CameraClient(BoxLayout):
+    source = StringProperty(None)
     def __init__(self, path):
         super(CameraClient, self).__init__()
         self.cwd = path
-        self.ids.path_label.text = self.cwd
-        self.img = 'image.jpg'
+        self.img = ''
+        self.url = ''
+
+    def clean(self):
+        self.img = ''
+        self.ids.results_label.text = ''
 
     def do_capture(self):
+        self.clean()
         name = name_imgs()
         ext = '.jpg'
         filename = "{}{}".format(name, ext)
         filepath = os.path.join(self.cwd, filename)
-
         try:
             camera.take_picture(filename=filepath,
                                 on_complete=self.camera_callback)
         except NotImplementedError:
-            popup = MsgPopup(
-                "This feature has not yet been implemented for this platform.")
+            # self.camera_callback('image.jpg')
+            popup = MsgPopup("This feature is not implemented for this platform.")
             popup.open()
 
     def camera_callback(self, filepath):
-        self.ids.msg_label.text = filepath
+        # to show the captured image
+        self.source = filepath
+        self.ids.image.reload()
+        print(self.source)
         self.img = filepath
+
+    def set_server(self):
+        self.url = self.ids.server_ip.text
+        print(self.url)
 
     def send_image(self):
         # if self.post_image(self.img) == 0:
@@ -49,21 +63,27 @@ class CameraClient(BoxLayout):
         #     self.ids.results_label.text = "Take a picture first"
         # else:
         #     self.ids.results_label.text = "Error to establish a connection"
-        self.post_image(self.img)
-        self.ids.results_label.text = self.img
+        result = self.post_image()
+        if result:
+            self.ids.results_label.text = result
 
-
-    def post_image(self, img_file):
-        # url for testing purposes
-        url = "http://localhost:5000/upload"
-        img = open(img_file, 'rb')
-        img = {'image': img}
+    def post_image(self):
+        if self.url == '':
+            popup = MsgPopup("Set server IP first")
+            popup.open()
+            return
+        # url for testing purposes 
+        url = "http://{}:5000/upload".format(self.url)
         try:
+            img = open(self.img, 'rb')
+            img = {'image': img}
             response = requests.post(url, files=img) 
-            print(response.text)
+            return response.text
         except RequestException as e:
             print("Error to establish a connection")
-
+        except IOError as e:
+            print("Could not read file")    
+        
 
 class CameraClientApp(App):
     def __init__(self):
